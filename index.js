bdcdb30e946a13b1f002d0d2ea9587af25edc8ce
St0neofFr33dom <christophefreeston@googlemail.com>
2022-09-08 20:41:43 +0100

    saved port change

--- index.js
+++ index.js
@@ -41,8 +41,8 @@ const startServer = async function(){
       );
       
 
-    app.listen({ port: 4000 }, () =>
-      console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+    app.listen({ port: process.env.PORT || 4000 }, () =>
+      console.log(`🚀 Server ready at ${url}`)
     );
 }
 
